990f7e7d273ea0e6baa0b1e0c7e920e21b0d2a5e
chris <christianwilliamsjire@gmail.com>
2019-09-15 11:30:52 +0300

    this commit will add Registration Form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators= [DataRequired(), Length(min =  5, max = 15)])
+    email = StringField('Email', validators= [DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min =  5, max = 15)])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
