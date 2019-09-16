17db980718b504855443f61e0b1a4d190d410b47
chris <christianwilliamsjire@gmail.com>
2019-09-16 17:27:09 +0300

    deploy

--- config.py
+++ config.py
@@ -17,10 +17,8 @@ class Config:
     
     
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://chris:matthewwilliams@localhost/pitch_app'
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://chris:matthewwilliams@localhost/pitch_app'
